65d857fd24214130231643d659f9af85c6fdd106
Dung Nguyen Anh (FE FPL HN) <DungNA29@fe.edu.vn>
2021-04-15 13:55:04 +0700

    1

---
+++ PT16307_UD_NET101_SP21_BLOCK2/Bai_3.3_KeThua/ClassChau.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai_3._3_KeThua
+{
+    class ClassChau:ClassCon
+    {
+        private string thuoctinhChau1;
+
+        public ClassChau()
+        {
+            
+        }
+
+        public ClassChau(string thuocTinhCha1, string thuocTinhCha2, string thuocTinhCha3, string thuocTinhCon1, string thuocTinhCon2, string thuoctinhChau1) : base(thuocTinhCha1, thuocTinhCha2, thuocTinhCha3, thuocTinhCon1, thuocTinhCon2)
+        {
+            this.thuoctinhChau1 = thuoctinhChau1;
+        }
+
+        public string ThuoctinhChau1
+        {
+            get => thuoctinhChau1;
+            set => thuoctinhChau1 = value;
+        }
+
+        public override void methodCha1()
+        {
+            base.methodCha1();
+        }
+
+        public override void methodCha2()
+        {
+            base.methodCha2();
+        }
+    }
+}
